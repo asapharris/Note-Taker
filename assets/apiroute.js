41d2fb2bf3a2b5b64648e0b19103e4d1a2ac16d1
Jamie <asapharris88@gmail.com>
2022-12-13 20:09:07 -0500

    refactoring code

---
+++ assets/apiroute.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs')
+var uuid = require('uuid');
+
+module.exports = (app) => {
+
+    app.get('/public/notes.html', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid(),
+        };
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let deleteNotes = db.filter(item => item.id !== req.params.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
+    })
+};
